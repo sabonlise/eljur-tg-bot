a8bcc958fa33a06b9a90a2c572d0c69e88dc88f8
sabonlise <46754745+sabonlise@users.noreply.github.com>
2020-05-06 00:26:18 +0200

    updates users model

--- data/users.py
+++ data/users.py
@@ -9,12 +9,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
 
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    chat_hash = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hash = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
-    telegram_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    telegram_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
