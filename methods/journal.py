773b0fef077e12bd7526a8dcfd236d1acfbe1130
sabonlise <46754745+sabonlise@users.noreply.github.com>
2020-05-05 19:15:38 +0200

    get previous and next weeks

--- methods/journal.py
+++ methods/journal.py
@@ -2,25 +2,47 @@ from methods.authorization import auth
 from bs4 import BeautifulSoup
 import requests
 import re
+import codecs
 
 
-def get_full_journal_week(session: requests.Session):
-    journal_app = session.get('https://gym40.eljur.ru/journal-app')
+def read_file(filename):
+    fileObj = codecs.open(filename, "r", "utf_8_sig")
+    text = fileObj.read()
+    return text
+
+
+def get_full_journal_week(session: requests.Session, week=None):
+    """Получить содержимое дневника за неделю,
+       указанную в параметре week (по умолчанию текущая неделя)
+       week = -1  дневник за предыдущую неделю
+       week = 1   дневник за следующую неделю"""
+    # journal_app = read_file('methods/prev_week.txt')
+    # soup = BeautifulSoup(journal_app, 'lxml')
+    if not week:
+        journal_app = session.get('https://gym40.eljur.ru/journal-app')
+    else:
+        journal_app = session.get(f'https://gym40.eljur.ru/journal-app/week.{-int(week)}')
     soup = BeautifulSoup(journal_app.text, 'lxml')
     journal = soup.find('div', id='dnevnikDays').text.split()
     return journal
 
 
 def get_week_days(journal: list):
-    days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота']
-    week_days = []
-    for lesson_day in range(len(journal)):
-        for day in days:
-            if journal[lesson_day].lower().startswith(day):
-                week_days.append(f'{journal[lesson_day]} {journal[lesson_day + 1]}')
-    return week_days
+    return re.findall(r'\w+, \d{2}\.\d{2}', ' '.join(journal))
 
 
-# testing regexp
-def get_journal(journal: list) -> list:
-    return re.split(r' [1-9][.]', ' '.join(journal))
+def get_lessons(full_journal: list) -> dict:
+    journal = re.split(r'(\w+, \d{2}\.\d{2})', ' '.join(full_journal))
+    lessons = {}
+    del journal[0]
+    journal = [day.strip() for day in journal]
+    for day in range(len(journal)):
+        try:
+            homework = re.split(r'(?= \d\.)', journal[day + 1])
+            homework = [hw.strip() for hw in homework]
+            lessons[journal[day]] = homework
+            journal.pop(day)
+            # del journal[day]
+        except IndexError as IE:
+            pass
+    return lessons
