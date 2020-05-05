61336c669e46769143ca104271dc476435b8514a
sabonlise <46754745+sabonlise@users.noreply.github.com>
2020-05-05 19:16:53 +0200

    added example

--- parser.py
+++ parser.py
@@ -1,15 +1,22 @@
 from methods.authorization import auth
 from methods.journal import *
 import requests
+import re
 
 
 def main():
     session = requests.Session()
-    auth(session, login, password)
-    full_journal = get_full_journal_week(session)
-    week_days = get_week_days(full_journal)
-    journal = get_journal(full_journal)
-    print(full_journal, week_days, journal, sep='\n')
+    auth(session, 'login', 'password')
+    full_journal = get_full_journal_week(session, -1)
+    # week_days = get_week_days(full_journal)
+    lessons_dictionary = get_lessons(full_journal)
+    day = ''     # день, который введет пользователь
+    for days, lessons in lessons_dictionary.items():
+        days = days.lower()
+        if day in days:
+            print(f'Расписание за {days}:')
+            for lesson in lessons:
+                print(lesson)
 
 
 if __name__ == '__main__':
