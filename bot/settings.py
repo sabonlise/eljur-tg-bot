099dd7bdc3ec0204c6780688204c3d53f29ac5d9
sabonlise <46754745+sabonlise@users.noreply.github.com>
2020-05-06 13:35:11 +0200

    configure proxy server

--- bot/settings.py
+++ bot/settings.py
@@ -1,1 +1,13 @@
-TOKEN = ''   # insert your bot token here
+# insert your bot token here
+TOKEN = '' 
+# configure your proxy server here
+REQUEST_KWARGS = {
+    'proxy_url': 'socks5://ip:port', # proxy server address
+    #  if auth needed:
+    # 'urllib3_proxy_kwargs': {
+    #     'assert_hostname': 'False',
+    #     'cert_reqs': 'CERT_NONE'
+    #     'username': 'user',
+    #     'password': 'password'
+    # }
+}
