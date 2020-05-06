5e47105d1ea3f3d420d06ae21881c64b515b7f81
sabonlise <46754745+sabonlise@users.noreply.github.com>
2020-05-06 21:51:42 +0200

    breakline fix

--- methods/journal.py
+++ methods/journal.py
@@ -1,48 +1,41 @@
-from methods.authorization import auth
 from bs4 import BeautifulSoup
 import requests
 import re
-import codecs
 
 
-def read_file(filename):
-    fileObj = codecs.open(filename, "r", "utf_8_sig")
-    text = fileObj.read()
-    return text
-
-
-def get_full_journal_week(session: requests.Session, week=None):
+def get_full_journal_week(session: requests.Session, week=0) -> list:
     """Получить содержимое дневника за неделю,
        указанную в параметре week (по умолчанию текущая неделя)
        week = -1  дневник за предыдущую неделю
        week = 1   дневник за следующую неделю"""
-    # journal_app = read_file('methods/prev_week.txt')
-    # soup = BeautifulSoup(journal_app, 'lxml')
     if not week:
         journal_app = session.get('https://gym40.eljur.ru/journal-app')
     else:
         journal_app = session.get(f'https://gym40.eljur.ru/journal-app/week.{-int(week)}')
     soup = BeautifulSoup(journal_app.text, 'lxml')
+    for br in soup.find_all('br'):
+        br.replace_with('breakline_key')
     journal = soup.find('div', id='dnevnikDays').text.split()
+    journal = [element.replace('breakline_key', '\n') for element in journal]
     return journal
 
 
-def get_week_days(journal: list):
+def get_week_days(journal: list) -> list:
     return re.findall(r'\w+, \d{2}\.\d{2}', ' '.join(journal))
 
 
 def get_lessons(full_journal: list) -> dict:
     journal = re.split(r'(\w+, \d{2}\.\d{2})', ' '.join(full_journal))
+    print(1, journal)
     lessons = {}
     del journal[0]
     journal = [day.strip() for day in journal]
     for day in range(len(journal)):
         try:
-            homework = re.split(r'(?= \d\.)', journal[day + 1])
+            homework = re.split(r'(?= \d\.[^\d])', journal[day + 1])
             homework = [hw.strip() for hw in homework]
             lessons[journal[day]] = homework
-            journal.pop(day)
-            # del journal[day]
+            del journal[day]
         except IndexError as IE:
             pass
     return lessons
