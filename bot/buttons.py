dffefcabdf7f671df3ef1967e7a3d06885f562c2
Maksimenko Aleksandr <S.maksimenko2004@mail.ru>
2020-05-07 14:02:50 +0200

    Update buttons.py

--- bot/buttons.py
+++ bot/buttons.py
@@ -3,14 +3,12 @@ from telegram import ReplyKeyboardMarkup
 
 
 BUTTON1_HELP = "Помощь"
-BUTTON2 = "Кнопка"
 
 
 def get_base_reply_keyboard():
     keyboard = [
         [
             KeyboardButton(BUTTON1_HELP),
-            KeyboardButton(BUTTON2),
         ],
     ]
     return ReplyKeyboardMarkup(
