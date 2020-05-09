d7083425970f65fa5fc3e2fccf2f8f8125ce7957
sabonlise <46754745+sabonlise@users.noreply.github.com>
2020-05-10 00:03:57 +0200

    Update schedule.py

--- bot/schedule.py
+++ bot/schedule.py
@@ -1,35 +1,4 @@
-from methods.crypto import password_decrypt
-from methods.authorization import auth
 from methods.journal import *
-
-from telegram.ext import CallbackContext
-from telegram import Update
-from data import db_session
-from data.users import User
-
-storage = {}
-
-
-def save_schedule(update: Update, context: CallbackContext) -> dict:
-    chat_id = update.effective_message.chat_id
-    state = storage[chat_id]['week_state']
-    try:
-        return storage[chat_id][state]
-    except KeyError:
-        session_db = db_session.create_session()
-        user = session_db.query(User).filter(User.telegram_id == chat_id).first()
-        session = requests.Session()
-        login = user.name
-        key = user.hash
-        password = password_decrypt(user.hashed_password, key).decode()
-        auth(session, login, password)
-        prev_week = get_full_journal_week(session, -1)
-        next_week = get_full_journal_week(session, 1)
-        current_week = get_full_journal_week(session)
-        storage[chat_id][-1] = get_lessons(prev_week)
-        storage[chat_id][1] = get_lessons(next_week)
-        storage[chat_id][0] = get_lessons(current_week)
-        return storage[chat_id][state]
 
 
 def save_formatted_schedule(match_day: str, schedule: dict) -> list:
@@ -42,7 +11,7 @@ def save_formatted_schedule(match_day: str, schedule: dict) -> list:
         for lesson in lessons:
             time = re.findall(r'\d\d:\d\d–\d\d:\d\d', lesson)
             # todo: фикс в случае непраивльного парсинга по времени
-            time = time[0] if time else 'оп ахах)'
+            time = time[0] if time else ''
             lesson = lesson.replace('\n', '\n\t\t\t\t\t').\
                 replace('Онлайн-урок завершен', '').replace('Онлайн-урок', '').split(' ')
             order = lesson[0]
