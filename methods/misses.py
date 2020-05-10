0dd3c342c7f883487973627fd5ff55f86f9ac074
sabonlise <46754745+sabonlise@users.noreply.github.com>
2020-05-11 01:44:23 +0200

    fixed parsing

--- methods/misses.py
+++ methods/misses.py
@@ -1,22 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-from methods.authorization import auth
-
-s = requests.Session()
 
 
-def misses(session: requests.Session, login, password) -> dict:
-    auth(session, login, password)
-    r = session.get('https://gym40.eljur.ru/journal-app/view.miss_report/&mode=excel')
-    soup = BeautifulSoup(r.content)
+def get_misses(session: requests.Session) -> dict:
+    r = session.get('https://gym40.eljur.ru/journal-app/view.miss_report')
+    soup = BeautifulSoup(r.content, 'lxml')
     miss = {}
-    subjects = soup.find_all('td', {'class': 'text-left'})[1:]
-    illness = soup.find_all('td', {'class': 'green miss_detail'})
-    respectfully = soup.find_all('td', {'class': 'lblue miss_detail'})
-    disrespectfully = soup.find_all('td', {'class': 'red miss_detail'})
+    subjects = soup.find_all('td', {'class': 'text-left'})
+    del subjects[0]
+    illness = soup.find_all('td', {'data-reason': 'b'})
+    respectfully = soup.find_all('td', {'data-reason': 'u'})
+    disrespectfully = soup.find_all('td', {'data-reason': 'n'})
     for i in range(len(subjects)):
-        mss = [str(illness[i]).replace('<td class="green miss_detail" data-reason="b">', '').replace('</td>', ''),
-               str(respectfully[i]).replace('<td class="lblue miss_detail" data-reason="u">', '').replace('</td>', ''),
-               str(disrespectfully[i]).replace('<td class="red miss_detail" data-reason="n">', '').replace('</td>', '')]
-        miss[str(subjects[i]).replace('<td class="text-left">', '').replace('</td>', '')] = mss
+        misses_list = [illness[i].text, respectfully[i].text, disrespectfully[i].text]
+        subject = str(subjects[i]).replace('<td class="text-left">', '').replace('</td>', '').strip()
+        if not subject:
+            subject = '\nВсего:'
+        miss[subject] = misses_list
     return miss
